b0eb113131a0ee84636d91adb2ef6bd9a784f4a6
Xeophan <70076006+Xeophan@users.noreply.github.com>
2020-08-31 17:50:18 +0300

    Product Details

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using acilsat_RB.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,16 @@ namespace acilsat_RB.Controllers
 {
     public class ProductsController : Controller
     {
+        acilsatDB db = new acilsatDB();
         // GET: Products
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult ProductDetail(int id)
+        {
+            Products product = db.Products.Find(id);
+            return View(product);
         }
     }
 }
